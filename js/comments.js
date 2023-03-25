5b97bcaf57910ff67bbdbca80045e752214bfc3e
TouhouXingChen <1799794493@qq.com>
2023-03-26 01:12:06 +0800

    Site updated: 2023-03-26 01:12:04

---
+++ js/comments.js
@@ -0,0 +1,8 @@
+<script type="module">
+    import { init } from 'https://unpkg.com/@waline/client@v2/dist/waline.mjs';
+
+    init(
+      el: '#waline',
+      serverURL: 'https://comments-iqbugxkdg-touhouxingchen.vercel.app'
+    );
+  </script>
---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// document.body.style.setProperty('--text-highlight-color', '#4bccff');
+//document.getElementById("article-container").style.color='#12b4e2';
+// 每秒执行一次修改任务
+// var interval = setInterval(function(){
+//     document.getElementById("article-container").style.color='#12b4e2';
+// },1000);
+// // 三秒后停止任务，减少进程
+// setTimeout(function (){
+//     clearInterval(interval);
+// },3000);
